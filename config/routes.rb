c8e50cc13f8a148d8d2706b3032c8f5dbf4914ae
chip-k <129578647+chip-k@users.noreply.github.com>
2023-04-04 12:21:08 +0000

    [Finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   # Defines the root path route ("/")
   # root "articles#index"
-  #devise_scope :user do
-    #get '/users/sign_out' => 'devise/sessions#destroy'
-  #end
-  
   get 'homes/about', to: 'homes#about', as: 'about'
 end
