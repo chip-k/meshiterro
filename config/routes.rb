4acc992dfc70b117c822f2b8ffc2639adfbe4fef
chip-k <129578647+chip-k@users.noreply.github.com>
2023-04-05 04:53:13 +0000

    [Finiah]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
